e135e611197b8baa97d343a53cd680117148fd50
orion <orion2003orion@gmail.com>
2024-08-29 19:06:38 +0300

    Added Product Item to LatestCollection

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react"
 import { ShopContext } from "../context/ShopContext"
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 
 const LatestCollection = () => {
@@ -21,6 +22,20 @@ const LatestCollection = () => {
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the.
         </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem 
+            key={index} 
+            id={item._id} 
+            image={item.image} 
+            name={item.name} 
+            price={item.price} />
+          ))
+        }
       </div>
     </div>
   )
