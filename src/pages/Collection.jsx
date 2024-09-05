ad53eee92fb3f89350d2a82f96d2f63f71e32352
orion <orion2003orion@gmail.com>
2024-09-05 15:20:12 +0300

    Added Sorting by price

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relevant');
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -34,16 +35,36 @@ const Collection = () => {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
 
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
+    }
+
     setFilterProducts(productsCopy);
   }
 
-  useEffect(() => {
-    setFilterProducts(products)
-  }, [])
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+
+    switch(sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+        break;
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=> (b.price - a.price)));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  }
 
   useEffect(() => {
     applyFilter();
   }, [category, subCategory])
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -89,7 +110,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2x1 mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value="relevant">Sort by: Relevant</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
