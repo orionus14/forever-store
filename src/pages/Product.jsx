595d33facd7d9ca2435b845e1af503bd008d1ae8
orion <orion2003orion@gmail.com>
2024-09-06 14:00:16 +0300

    Added sizes to Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -60,6 +60,14 @@ const Product = () => {
           </div>
           <p className='mt-5 text-3x1 font-medium'>{currency}{productData.price}</p>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item, index) => (
+                <button className={`border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
